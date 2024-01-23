3215d8db49e2e95c8e22b8ee2e2d964314509555
prasad-rup <prasad.kandekar.in@rupeek.com>
2024-01-23 14:35:48 +0530

    Added user name in Profile Page

--- src/components/ProfileComponents/DeleteUser.js
+++ src/components/ProfileComponents/DeleteUser.js
@@ -2,16 +2,32 @@
 import React from 'react';
 import './DeleteUser.css';
 
-const DeleteUser = ({ onDeleteUser }) => {
-  const handleDeleteUser = () => {
-    // Add your logic to handle user deletion
-    // You may want to make an API call to delete the user
-    onDeleteUser();
+const DeleteUser = ({ onDeleteUser, userId }) => {
+  const handleDeleteUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/users/${userId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        onDeleteUser();
+        console.log(data.message);
+      } else {
+        const data = await response.json();
+        console.error(data.error || 'Error deleting user');
+      }
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
   };
 
   return (
     <div className="delete-user-container">
-      <h2>Delete User</h2>
+      <h2>Delete Account</h2>
       <p>Are you sure you want to delete your account? This action cannot be undone.</p>
       <button onClick={handleDeleteUser} className="delete-user-button">
         Delete Account
